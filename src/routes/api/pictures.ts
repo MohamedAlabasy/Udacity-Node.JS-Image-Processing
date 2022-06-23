5199ea41ec7f79d2690119a4c69045b7b7dda7c2
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-23 14:01:27 +0200

    add check Pictures Exists

--- src/routes/api/pictures.ts
+++ src/routes/api/pictures.ts
@@ -26,8 +26,4 @@ pictures.get('', async (request: Request, response: Response, next: NextFunction
 
 
 
-
-
-
-
 export default pictures;
--- src/routes/api/utilities/validateData.ts
+++ src/routes/api/utilities/validateData.ts
@@ -1,6 +1,22 @@
+import path from 'path';
+import { promises as fsPromises } from 'fs';
+
+const FULL_PATH = path.resolve(__dirname, '../../../../assets/full');
+
 async function validateData(_name: string, _width: string, _height: string): Promise<null | string> {
     let msg: null | string = null;
 
+    // #=======================================================================================#
+    // #			                        check Pictures Exists                              #
+    // #=======================================================================================#
+    if ((await isPicturesExists(_name)) === false) {
+        const availablePictures: string = (await getAvailablePictures()).join(' | ');
+        return `<h1>Available pictures ${availablePictures}</h1>`;
+    }
+
+    // #=======================================================================================#
+    // #			                        check Pictures width                               #
+    // #=======================================================================================#
     if (!_width) {
         msg = "You must enter width";
     } else if (+_width < 1) {
@@ -10,7 +26,9 @@ async function validateData(_name: string, _width: string, _height: string): Pro
         msg = "width must be number";
     }
 
-
+    // #=======================================================================================#
+    // #			                        check Pictures height                              #
+    // #=======================================================================================#
     if (!_height) {
         msg = "You must enter height";
     } else if (+_height < 1) {
@@ -19,8 +37,19 @@ async function validateData(_name: string, _width: string, _height: string): Pro
     else if (Number.isNaN(+_height)) {
         msg = "height must be number";
     }
+
     return msg;
 };
 
 
+async function isPicturesExists(_name: string = ''): Promise<boolean> {
+    if (!_name) return false;
+    return (await getAvailablePictures()).includes(_name);
+}
+
+async function getAvailablePictures(): Promise<string[]> {
+    const AvailablePictures: string[] = (await fsPromises.readdir(FULL_PATH))
+        .map((picturesName: string): string => picturesName.split('.')[0]);
+    return AvailablePictures;
+}
 export default validateData
