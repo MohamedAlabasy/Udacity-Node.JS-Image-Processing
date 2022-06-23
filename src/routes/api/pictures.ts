4d1d5822ad3b991158bb3202d1a29bd81975029e
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-23 14:57:15 +0200

    add git ignore

--- src/routes/api/pictures.ts
+++ src/routes/api/pictures.ts
@@ -11,25 +11,58 @@ const THUMB_PATH = path.resolve(__dirname, '../../../assets/thumb');
 
 
 pictures.get('', async (request: Request, response: Response, next: NextFunction) => {
-
     let dataSendToUser: null | string;
 
     const pictureName: string = request.query.name as string;
     const pictureWidth: string = request.query.width as string;
     const pictureHeight: string = request.query.height as string;
 
-    // Make sure the data is correct
+    // Make sure the data is correct else return (h1 = error massage)
     dataSendToUser = await validateData(pictureName, pictureWidth, pictureHeight);
     if (dataSendToUser) response.send(`<h1>${dataSendToUser}</h1>`);
 
-    // check picture Thumb
+    // check if picture in Thumb and create picture if isn't in Thumb
     if (await isPictureInThumb(pictureName, pictureWidth, pictureHeight) === false) {
         dataSendToUser = await createPictureThumb(pictureName, pictureWidth, pictureHeight);
     }
     if (dataSendToUser) response.send(`<h1>${dataSendToUser}</h1>`);
 
+    // get Image Path and send it to user
+    dataSendToUser = await getPicturePath(pictureName, pictureWidth, pictureHeight);
+    dataSendToUser ? response.sendFile(dataSendToUser) : response.send('<h1>Error</h1>');
 })
 
+/**
+ * get picture path
+ * @param _name name of picture
+ * @param _width width of picture
+ * @param _height height of picture
+ * @returns path of Picture or null if Picture isn't exists
+ */
+async function getPicturePath(_name: string, _width: string, _height: string): Promise<null | string> {
+    let picturePath: string;
+
+    if (_width && _height) {
+        picturePath = path.resolve(THUMB_PATH, `${_name}_${_width}X${_height}.jpg`)
+    } else {
+        picturePath = path.resolve(FULL_PATH, `${_name}.jpg`);
+    }
+
+    try {
+        await fsPromises.access(picturePath);
+        return picturePath;
+    } catch {
+        return null;
+    }
+}
+
+/**
+ * check if Picture is in Thumb or not
+ * @param _name name of picture
+ * @param _width width of picture
+ * @param _height height of picture
+ * @returns true if Picture is in Thumb or false if not
+ */
 async function isPictureInThumb(_name: string, _width: string, _height: string): Promise<boolean> {
     try {
         await fsPromises.access(path.resolve(THUMB_PATH, `${_name}_${_width}X${_height}.jpg`));
@@ -39,6 +72,13 @@ async function isPictureInThumb(_name: string, _width: string, _height: string):
     }
 }
 
+/**
+ * add Picture to Thumb
+ * @param _name name of picture
+ * @param _width width of picture
+ * @param _height height of picture
+ * @returns null if Picture created or error if some thing happened
+ */
 async function createPictureThumb(_name: string, _width: string, _height: string): Promise<null | string> {
     try {
         await sharp(path.resolve(FULL_PATH, `${_name}.jpg`))
