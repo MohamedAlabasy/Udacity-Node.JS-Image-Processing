4672a24d989ac8578dea6b9a8f21d69510bc3299
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-23 02:50:33 +0200

    add spec

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -3,11 +3,18 @@ import sharp from 'sharp';
 import path from 'path';
 import { promises as fsPromises } from 'fs';
 
-
-
 const images: Router = Router()
+const fullImagePath = path.relative(__dirname, '../../../assets/full');
+const thumbImagePath = path.relative(__dirname, '../../../assets/full');
 
 images.get('', (request: Request, response: Response, next: NextFunction) => {
+    const imageName: string = request.query.name as string;
+    const imageWidth: number = (request.query.width as unknown) as number;
+    const imageHight: number = (request.query.hight as unknown) as number;
+    
+
+
+
     response.send('images')
 })
 
